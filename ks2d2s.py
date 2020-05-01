60470a2d1e2c8c136e882ecc21d762a93ef32ded
spencer <spencerscott@email.arizona.edu>
2020-04-30 22:37:40 -0700

    oops, last commit shouldn't have happened. implemented actual quadct

--- ks2d2s.py
+++ ks2d2s.py
@@ -4,8 +4,8 @@ from scipy import stats
 def ks2d2s(x1, y1, x2, y2):
     assert (len(x1) == len(y1)) & (len(x2) == len(y2))
 
-    d1 = quadct(x1, y1, x2, y2)
-    d2 = quadct(x2, y2, x1, y1)
+    d1 = maxdist(x1, y1, x2, y2)
+    d2 = maxdist(x2, y2, x1, y1)
     d = np.mean(d1, d2)
     sqen = np.sqrt(n1 * n2 / (n1+n2))
     r1 = stats.pearsonr(x1, y1)[0]
@@ -15,15 +15,29 @@ def ks2d2s(x1, y1, x2, y2):
     return p
 
 
+def quadct(x, y, xx, yy):
+    nn  = len(xx)
+    na = np.sum((yy > y) & (xx > x))
+    nb = np.sum((yy > y) & (xx < x))
+    nc = np.sum((yy < y) & (xx < x))
+    nd = np.sum((yy < y) & (xx > x))
 
-def quadct(x1, y1, x2, y2)
+    ff = 1 / nn
+    fa = na * ff
+    fb = nb * ff
+    fc = nc * ff
+    fd = nd * ff
+    return fa, fb, fc, fd
+
+
+def maxdist(x1, y1, x2, y2):
     n1 = len(x1)
 
     d1 = 0
     n1_inv = 1 / n1
 
     for i in range(n1):
-        fa, fb, fc, fd = quadct(x1[i]. y1[i], x1, y1)
+        fa, fb, fc, fd = quadct(x1[i], y1[i], x1, y1)
         ga, gb, gc, gd = quadct(x1[i], y1[i], x2, y2)
         if fa > ga:
             fa += n1_inv
